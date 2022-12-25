1805e11b1f8a5072be74c4c02fd7af6a1e47c38b
Christopher <christopherraj@jmangroup.com>
2022-12-25 12:14:12 +0530

    fix in Products API

--- src/Component/Products.js
+++ src/Component/Products.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
-import list from "../data";
+import React, { useState, useEffect } from "react";
+// import list from "../data";
 import { Cards } from "./Card";
+import axios from "axios";
 
 export const Products = ({ handleClick }) => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const data = await axios.get("http://localhost:9000/products-list");
+    console.log(data.data);
+    setData(data.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <section>
-      {list.map((item) => (
+      {data.map((item) => (
         <Cards key={item.id} item={item} handleClick={handleClick} />
       ))}
     </section>
